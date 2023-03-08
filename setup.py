ba03a12869ca320950b410a8e3bb145eeb3fccfc
Pierre Gibertini <pierre.gibertini@etu.utc.fr>
2023-03-08 23:13:43 +0000

    style

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='3d-IFu-letters',
-    version='0.1',
-    description='3D modeling of letters of Imaginarium Festival',
-    author='Pierre Gibertini',
-    packages=['3dletters'],
-    install_requires=[
-        'dessia_common>=0.13.2',
-        'volmdlr>=0.9.0'
-    ],
+    name="3d-IF-letters",
+    version="0.1",
+    description="3D modeling of letters of Imaginarium Festival",
+    author="Pierre Gibertini",
+    packages=["3dletters"],
+    install_requires=["dessia_common>=0.13.2", "volmdlr>=0.9.0"],
 )
